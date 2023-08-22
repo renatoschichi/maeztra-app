99356b344625e180da8aca33c52bc9f19c52c6d4
Renato Schichi <renatoschichi@gmail.com>
2023-08-22 10:09:18 -0300

    newsletter component finished + footer first steps

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,7 +2,26 @@ import './style.scss'
 
 function Footer() {
   return (
-    <div></div>
+    <div className="maeztra-footer">
+      <div className="maeztra-footer-left">
+        <span>Informações</span>
+        <span>Quem Somos</span>
+        <span>Prazo de Envio</span>
+        <span>Trocas e Devoluções</span>
+        <span>Promoções e Cupons</span>
+      </div>
+      <div className="maeztra-footer-middle">
+      <span>Minha Conta</span>
+      <span>Minha Conta</span>
+      <span>Meus Pedidos</span>
+      <span>Cadastre-se</span>
+      </div>
+      <div className="maeztra-footer-right">
+      <span>Onde nos encontrar</span>
+      <span>Lojas</span>
+      <span>Endereço</span>
+      </div>
+    </div>
   )
 }
 
--- src/components/newsletter/index.jsx
+++ src/components/newsletter/index.jsx
@@ -1,8 +1,57 @@
+import React, { useState } from 'react'
+import InputMask from 'react-input-mask'
+import { ToastContainer, toast } from 'react-toastify'
+
+import 'react-toastify/dist/ReactToastify.css'
 import './style.scss'
 
 function Newsletter() {
+  const [email, setEmail] = useState('')
+
+  const handleSendEmail = () => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!emailPattern.test(email)) {
+      toast.error('Por favor, insira um e-mail válido.', {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      })
+      return
+    }
+
+    setTimeout(() => {
+      toast.success('E-mail enviado com sucesso!', {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+      setEmail('')
+    }, 1000)
+  }
+
   return (
-    <div></div>
+    <div className="maeztra-newsletter">
+      <label>Receba Nossa Newsletter</label>
+      <div className="maeztra-input">
+        <InputMask
+          mask=""
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          placeholder="Digite seu e-mail"
+        />
+        <button className="maeztra-btn maeztra-btn-send" onClick={handleSendEmail}>
+          Enviar
+        </button>
+        <ToastContainer />
+      </div>
+    </div>
   )
 }
 
